3819579a26274fd08cbb2cf7de1d9929b5960a87
inb <inorton@gmail.com>
2025-07-15 13:57:19 +0100

    Its a framework now!

---
+++ content/scripts/library_custom_5.lua
@@ -0,0 +1,176 @@
+-- common functions
+c_input_left = 0
+c_input_right = 1
+color_white = color8(255, 255, 255, 255)
+
+function beep()
+    update_play_sound(9)
+end
+
+function beep2()
+    update_play_sound(7)
+end
+
+-- base class for game app
+local App = {}
+App.__index = App
+AppBase = App
+function App.new()
+    local self = setmetatable({}, App)
+    self.screen_w = 320
+    self.screen_h = 240
+    self.update_func = nil
+    self.input_axis_func = nil
+    self.input_event_func = nil
+    self.input_scroll_func = nil
+    self.input_pointer_func = nil
+    return self
+end
+function App:update(screen_w, screen_h, t)
+    self.screen_w = screen_w
+    self.screen_h = screen_h
+    if self.update_func then
+        self.update_func(self.screen_w, self.screen_h, t)
+    end
+end
+
+function App:input_event(a, b)
+    if self.input_event_func then
+        self.input_event_func(a, b)
+    end
+end
+
+function App:input_axis(x, y, z, w)
+    if self.input_axis_func then
+        self.input_axis(x, y, z, w)
+    end
+end
+
+function App:input_pointer(is_hovered, x, y)
+    if self.input_pointer_func then
+        self.input_pointer_func(is_hovered, x, y)
+    end
+end
+
+function App:input_scroll(dy)
+    if self.input_scroll_func then
+        self.input_scroll_func(dy)
+    end
+end
+
+-- game selector
+local Selector = {}
+Selector.__index = AppBase
+setmetatable(Selector, AppBase)
+function Selector.new()
+    local self = setmetatable({}, {__index = Selector})
+    self.ui = lib_imgui:create_ui()
+    return self
+end
+
+function Selector:update(w, h, t)
+    self.screen_w = w
+    self.screen_h = h
+    self.ui:begin_ui()
+    self.ui:begin_window("Select App", 10, 10, w - 20, h - 20, nil, true, 0, true, true)
+    self.ui:header("Apps")
+    if self.ui:button("Vehicle Control", true, 1) then
+        g_current_app = "control_screen"
+    end
+    if self.ui:button("Breakout", true, 1) then
+        g_current_app = "breakout"
+    end
+
+    self.ui:end_window()
+    self.ui:end_ui()
+end
+
+function Selector:input_event(e, a)
+    self.ui:input_event(e, a)
+end
+
+function Selector:input_pointer(h, x, y)
+    self.ui:input_pointer(h, x, y)
+end
+
+
+if begin then
+    old_begin = begin
+end
+
+default_begin_load = begin_load
+default_update = nil
+
+g_apps = {}
+g_app_load_err = nil
+g_current_app = "selector"
+
+function game_begin()
+    if default_update == nil then
+        default_update = update
+        default_input_scroll = input_scroll
+        default_input_event = input_event
+        default_input_axis = input_axis
+        default_input_pointer = input_pointer
+    end
+
+    if g_tut_is_carrier_selected ~= nil and g_app_load_err == nil then
+        -- this is a control screen, install our game selector
+        update = wrapped_update
+        input_event = wrapped_input_event
+        input_pointer = wrapped_input_pointer
+    end
+end
+
+function begin_load()
+    default_begin_load()
+    game_begin()
+end
+
+g_last_power = g_is_on
+
+function wrapped_update(screen_w, screen_h, ticks)
+    if g_is_on ~= g_last_power then
+        g_last_power = g_is_on
+        if g_is_on then
+            -- powered off and back on
+            g_current_app = "selector"
+        end
+    end
+
+    if g_apps["selector"] == nil then
+        g_apps["selector"] = Selector:new()
+        g_apps["control_screen"] = App:new()
+        g_apps["control_screen"].update_func = default_update
+        g_apps["control_screen"].input_scroll_func = default_input_scroll
+        g_apps["control_screen"].input_event_func = default_input_event
+        g_apps["control_screen"].input_axis_func = default_input_axis
+        g_apps["control_screen"].input_pointer_func = default_input_pointer
+        print("apps loaded")
+    end
+    local app = g_apps[g_current_app]
+    local st, err = pcall(function()
+        if app ~= nil then
+            app:update(screen_w, screen_h, ticks)
+        else
+            update = default_update
+        end
+    end)
+    if not st then
+        print(err)
+        update = default_update
+    end
+end
+
+function wrapped_input_event(e, a)
+    local app = g_apps[g_current_app]
+    app:input_event(e, a)
+end
+function wrapped_input_pointer(is_hovered, x, y)
+    local app = g_apps[g_current_app]
+    app:input_pointer(is_hovered, x, y)
+end
+function wrapped_input_scroll(dy)
+    local app = g_apps[g_current_app]
+    app:input_scroll(dy)
+end
---
+++ content/scripts/library_custom_6.lua
@@ -0,0 +1,179 @@
+-- Breakout game
+local GameBreakout = {}
+GameBreakout.__index = AppBase
+setmetatable(GameBreakout, AppBase)
+function GameBreakout.new()
+    local self = setmetatable({}, {__index = GameBreakout})
+    self.paddle_dx = 0
+    self.paddle_x = 0
+    self.paddle_y = 0
+    self.paddle_w = 24
+    self.ball_y = 0
+    self.ball_x = 0
+    self.ball_dy = 3
+    self.ball_dx = 1
+    self.row_dy = 8
+    self.col_dx = 4
+    self.block_h = 8
+    self.block_w = 16
+    self.rows = {}
+    self.remaining = -1
+    -- margins
+    self.mx = 10
+    self.my = 10
+    self:reset()
+    return self
+end
+
+function GameBreakout:input_event(event, action)
+    if action == e_input_action.press then
+        if event == e_input.back then
+            update_set_screen_state_exit()
+        else
+            if event == c_input_left then
+                self.paddle_dx = -5
+            elseif event == c_input_right then
+                self.paddle_dx = 5
+            end
+        end
+    elseif action == e_input_action.release then
+        if event ~= e_input.back then
+            self.paddle_dx = 0
+        end
+    end
+end
+
+function GameBreakout:reset()
+    self.rows = {
+        {1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1},
+    }
+    self.remaining = -1
+    self.ball_y = self.screen_h
+end
+
+function GameBreakout:check_hit_block()
+    local bx = self.ball_x
+    local by = self.ball_y
+
+    if by < self.screen_h / 2 then
+        for i = 1, #self.rows do
+            for j = 1, #self.rows[i] do
+                local block = self.rows[i][j]
+                if block == 1 then
+                    local x1 = ((self.col_dx + self.block_w) * j) - self.block_w / 2
+                    local x2 = x1 + self.block_w
+                    local y1 = ((self.row_dy + self.block_h) * i) - 1
+                    local y2 = y1 + self.block_h
+
+                    if by >= y1 and by <= y2 then
+                        if bx >= x1 and bx <= x2 then
+                            -- hit, delete the block
+                            self.rows[i][j] = 0
+                            return true
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    return false
+end
+
+function GameBreakout:update(screen_w, screen_h, ticks)
+    -- check reset
+    self.screen_w = screen_w - self.mx * 2
+    self.screen_h = screen_h - self.my * 2
+    if self.remaining <= 0 then
+        self:reset()
+    end
+    --update_ui_push_alpha(32)
+    update_ui_push_offset(self.mx, self.my)
+    -- update_ui_push_clip(0, 0, screen_w - 20, screen_h - 30)
+    --update_ui_push_scale(2)
+
+    -- move the ball
+    self.ball_x = self.ball_x + self.ball_dx
+    self.ball_y = self.ball_y + self.ball_dy
+
+    if self.ball_x < 0 or self.ball_x > screen_w - self.mx * 2 then
+        self.ball_dx = self.ball_dx * -1
+        beep()
+    --elseif self.ball_x > screen_w then
+      --  self.ball_dx =  self.ball_dx * -1
+    end
+
+    if self.ball_y < 1 then
+        self.ball_y = 1
+        self.ball_dy = self.ball_dy * -1
+        if self.ball_dy > 5 then
+            self.ball_dy = 5
+        end
+        beep()
+    end
+
+    if self.ball_y > (screen_h + 10) then
+        self.ball_dy = self.ball_dy * -1
+    end
+    self.paddle_y = math.floor(screen_h * 0.8)
+
+    -- detect paddle hit ball
+    if self.ball_dy > 0 then
+        -- moving down
+        local d_paddle_x = math.abs(self.paddle_x - self.ball_x)
+
+        local d_paddle_y = math.abs(self.paddle_y - self.ball_y)
+
+        if d_paddle_y < 5 then
+            if d_paddle_x < self.paddle_w then
+                self.ball_dy = self.ball_dy * -1
+                if d_paddle_x < 5 then
+                    self.ball_dx = self.ball_dx * 0.7
+                    self.ball_dy = self.ball_dy * 1.3
+                end
+            end
+        end
+    end
+
+    if self:check_hit_block() then
+        beep2()
+        self.ball_dy = self.ball_dy * -1
+    end
+
+    -- move paddle
+    self.paddle_x = self.paddle_x + self.paddle_dx
+
+    if self.paddle_x < 0 then
+        self.paddle_x = 1
+    elseif self.paddle_x > screen_w - (2 * self.mx) then
+        self.paddle_x = screen_w - (2 * self.mx)
+    end
+
+    -- draw the paddle
+    update_ui_rectangle(self.paddle_x - math.floor(self.paddle_w / 2), self.paddle_y, self.paddle_w, 2, color_white)
+
+    -- draw the ball
+    update_ui_rectangle(self.ball_x -2, self.ball_y -2, 4, 4, color_white)
+
+    -- draw the blocks
+    self.remaining = 0
+    for i = 1, #self.rows do
+        for j = 1, #self.rows[i] do
+            local block = self.rows[i][j]
+            if block == 1 then
+                self.remaining = self.remaining + 1
+                local r = (i * 137) % 253
+                local g = (j * 45) % 201
+                local b = (j * i * 3) % 255
+                local color = color8(r, g, b, 255)
+                update_ui_rectangle(
+                        (self.col_dx + self.block_w) * j - self.block_w / 2,
+                        (self.row_dy + self.block_h) * i, self.block_w, self.block_h, color)
+            end
+        end
+    end
+end
+
+g_apps["breakout"] = GameBreakout:new()
